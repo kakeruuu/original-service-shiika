3fa79ef6990835a7908f52b985068214250bb824
suzuki kakeru <kakeruuuuuu0114.2@gmail.com>
2020-08-01 09:06:31 +0000

    Readme create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
     
     def require_user_logged_in
         unless logged_in?
+            flash[:info] = 'その操作はログイン時のみ可能です'
             redirect_to login_url
         end
     end
